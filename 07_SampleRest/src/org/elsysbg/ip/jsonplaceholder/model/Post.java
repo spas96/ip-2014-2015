797c86bd3a02ed29fdc9a4b87731859ec394b10b
spas96 <s.spasov96@abv.bg>
2015-03-11 18:11:25 +0200

    Sample model from http://jsonplaceholder.typicode.com/

---
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
@@ -0,0 +1,40 @@
+package org.elsysbg.ip.jsonplaceholder.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Model is copied by http://jsonplaceholder.typicode.com/
+ */
+@XmlRootElement
+public class Post {
+	
+	private long id;
+	private User user;
+	private String title;
+	private String body;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
